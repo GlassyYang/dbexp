7d630bd0df1146851da8ff84c746aab22589d0be
GlassyYang <zhangya2009@live.com>
2019-05-07 23:36:36 -0400

    commit daitly

--- dbexp2/btree.hpp
+++ dbexp2/btree.hpp
@@ -4,19 +4,19 @@
 #include <cstring>
 #include <stack>
 
-//#define ME_DEBUG
+#define ME_DEBUG
 //调试模式
 #ifndef ME_DEBUG
 
-#define BLOCKSIZE 1024
-#define MIDBLK 512
+#define BLOCKSIZE 1023
+#define MIDBLK 511
 #else
-#define BLOCKSIZE 4
+#define BLOCKSIZE 5
 #define MIDBLK 2
 #endif
 #define MID(a, b) (((a) + (b)) >> 1)
 #define CAN_INSERT(capacity) ((capacity) < BLOCKSIZE)
-#define CAN_DELETE(capacity) ((capacity) >= MIDBLK)
+#define CAN_DELETE(capacity) ((capacity) > MIDBLK)
 #define SIZE(a, T) ((a) * sizeof(T))
 namespace dbexp
 {
@@ -24,27 +24,29 @@ namespace dbexp
 template <class value_type>
 class comparitor
 {
-  public:
+public:
 	virtual int compare(value_type a, value_type b) = 0;
 };
 
 template <class value_type>
 class btree
 {
-  private:
+private:
 	struct bnode
 	{
 		value_type value[BLOCKSIZE];
 		struct bnode *child[BLOCKSIZE + 1];
 		int index;
-	} *root;
+	} * root;
 	comparitor<value_type> *comp;
-	int binary_search(struct bnode*, value_type);
-	void init_node(bnode*);
-	void insert(bnode*, int, value_type);
-	value_type remove(bnode*, int);
+	int binary_search(struct bnode *, value_type);
+	void init_node(bnode *);
+	void insert(bnode *, int, value_type);
+	value_type remove(bnode *, int);
 	void finalize(bnode *node);
-  public:
+	int adjust(bnode* parent, bnode *node, int index);
+
+public:
 	btree(comparitor<value_type> *comp);
 	~btree();
 	bool insert(value_type);
@@ -63,15 +65,16 @@ btree<T>::btree(comparitor<T> *comp)
 template <class T>
 btree<T>::~btree()
 {
-	finalize(root);
+	//finalize(root);
 }
 
-template<class T>
+template <class T>
 void btree<T>::finalize(bnode *node)
 {
-	for(int i = 0; i <= node->index; i++)
+	for (int i = 0; i <= node->index; i++)
 	{
-		if(node->child[i] != NULL){
+		if (node->child[i] != NULL)
+		{
 			finalize(node->child[i]);
 			node->child[i] = NULL;
 		}
@@ -116,16 +119,16 @@ bool btree<T>::insert(T e)
 		//然后扩展其父节点，将要插入的节点添加到其父节点中
 		bnode *buf = new bnode;
 		init_node(buf);
-		int cut = index >= MIDBLK ? MIDBLK + 1: MIDBLK;
-		memcpy((void*)(buf->value), (const void*)(cur->value + cut), SIZE(BLOCKSIZE - cut, T));
-		memcpy((void*)(buf->child), (const void*)(cur->child + cut), SIZE(BLOCKSIZE - cut + 1, struct bnode*));
+		int cut = index > MIDBLK ? MIDBLK + 1 : MIDBLK;
+		memcpy((void *)(buf->value), (const void *)(cur->value + cut), SIZE(BLOCKSIZE - cut, T));
+		memcpy((void *)(buf->child), (const void *)(cur->child + cut), SIZE(BLOCKSIZE - cut + 1, struct bnode *));
 		buf->index = cur->index - cut;
 		cur->index = cut;
-		memset(static_cast<void*>(cur->value + cut), 0, SIZE(BLOCKSIZE - cut, T));
-		memset(static_cast<void*>(cur->child + cut), 0, SIZE(BLOCKSIZE - cut + 1, struct bnode*));
-		if(index >= MIDBLK)
+		memset(static_cast<void *>(cur->value + cut), 0, SIZE(BLOCKSIZE - cut, T));
+		memset(static_cast<void *>(cur->child + cut), 0, SIZE(BLOCKSIZE - cut + 1, struct bnode *));
+		if (index > MIDBLK)
 		{
-			index -= MIDBLK;
+			index -= cut;
 			insert(buf, index, ins);
 			buf->child[index] = left;
 			buf->child[index + 1] = right;
@@ -138,12 +141,12 @@ bool btree<T>::insert(T e)
 		}
 		//在做好这一切之后，需要将当前的值插入到父节点的位置上，然后将中间的值作为新的值插入
 		//需注意的是，如果父节点是空的，则说明刚刚插入的节点就是父节点，需要创建新的父
-		ins = cur->value[cut];
+		ins = cur->value[MIDBLK];
 		cur->index -= 1;
 		left = cur;
 		right = buf;
 		//为cur和index装载新的值，为空说明需要添加一层
-		if(nodes.empty())
+		if (nodes.empty())
 		{
 			cur = new bnode;
 			init_node(cur);
@@ -177,7 +180,7 @@ bool btree<T>::find(T e)
 	{
 		//使用二分查找
 		int index = binary_search(cur, e);
-		if (index < 0)
+		if (index <= 0)
 		{
 			cur = cur->child[-index];
 		}
@@ -189,135 +192,94 @@ bool btree<T>::find(T e)
 	return false;
 }
 
-template<typename T>
+//TODO 删除操作
+template <typename T>
 bool btree<T>::remove(T e)
 {
 	//在删除的过程中，如果没有找到则返回false
 	using namespace std;
-	//copy要插入的值
-	T ins = e;
-	stack<bnode *> nodes;
-	stack<int> indexes;
 	bnode *cur = root;
+	bnode *parent;
+	int index;
 	while (cur != NULL)
 	{
-		nodes.push(cur);
-		int index = binary_search(cur, e);
+		index = binary_search(cur, e);
 		if (index <= 0)
 		{
-			cur = cur->child[-index];
-			indexes.push(index);
-			nodes.push(cur);
+			index = -index;
+			parent = cur;
+			cur = cur->child[index];
+			if(cur == NULL)
+			{
+				return false;
+			}
+			if (!CAN_DELETE(cur->index))
+			{
+				index = adjust(parent, cur, index);
+			}
 		}
 		else
 		{
-			index -= 1;		//当是正数的时候，返回的是原来的index + 1，所以需要-1.
-			cur = cur->child[index];
-			indexes.push(index);
-			nodes.push(cur);
+			index -= 1;
 			break;
 		}
 	}
-	if(cur == NULL)	//说明没有找到值
-	{
-		return false;
-	}
-	cur = nodes.pop();
-	int index = indexes.top();
-	indexes.pop();
+	parent = cur;
+	bnode *left, *right;
 	//如果找到的节点不是叶子节点，需要将删除的节点进行替换，选择前驱节点进行替换，因为前驱节点删除比较简单
-	if(cur->child[index] != NULL)	//不是叶子节点，是内部节点
+	while (cur->child[index] != NULL) //不是叶子节点，是内部节点
 	{
-		bnode *ntemp = cur;
-		int itemp = index;
-		index -= 1;
-		cur = cur->child[index];
-		indexes.push(index);
-		nodes.push(cur);
-		while(cur != NULL)
+		//这儿的left和right是一定不会NULL的。
+		left = cur->child[index];
+		right = cur->child[index + 1];
+		if (CAN_DELETE(left->index))
 		{
-			index = cur->index;
-			cur = cur->child[index];
-			indexes.push(index);
-			nodes.push(cur);
+			while (left != NULL)
+			{
+				if(!CAN_DELETE(left->index))
+				{
+					adjust(parent, left, right->index - 1);
+				}
+				parent = left;
+				left = left->child[index];
+			}
+			swap(cur->value[index], parent->value[parent->index - 1]);
+			parent->index -= 1;
+			return true;
 		}
-		//找到叶子节点之后，将之替换掉
-		cur = nodes.top();
-		index = indexes.top() - 1;
-		swap(cur->value[index], ntemp->value[itemp]);
-	}
-	cur = nodes.top();
-	nodes.pop();
-	index = indexes.top();
-	indexes.pop();
-	//函数至此已经到达了叶子节点。因为删除只在叶子节点中进行，所以不用回溯
-	while(cur != NULL && !CAN_DELETE(cur->index))
-	{
-		bnode *parent = nodes.top();
-		int pindex = nodes.top();
-		bnode *rb = (pindex + 1 < parent->index) ? parent[pindex + 1] : NULL;
-		bnode *lb = (pindex - 1 >= 0) ? parent[pindex + 1] : NULL;
-		//如果其右兄弟节点能够删除，则从其中删除最左边的节点移至父节点的当前位置上，将父节点上的值从其中
-		//移到将要删除的位置上
-		//调整cur，使指针移到末尾处
-		remove(cur, index);
-		index = cur->index;
-		cur->index++;
-		if(rb != NULL)
+		else if (CAN_DELETE(right->index))
 		{
-			cur->value[index] = parent->value[pindex];
-			if(CAN_DELETE(rb->index))
+			while (right != NULL)
 			{
-				parent->value[pindex] = remove(rb, 0);
-				cur = NULL;
+				if(!CAN_DELETE(right->index))
+				{
+					adjust(parent, right, right->index - 1);
+				}
+				parent = right;
+				right = right->child[index];
 			}
-			else
-			{
-				//进行合并
-				memcpy((void*)(cur->value + index), (void*)(rb->value), SIZE(rb->index, T));
-				//合并之后需要递归检查父节点是否满足要求
-				cur->index += rb->index;
-				delete rb;
-				cur = parent;
-				index = pindex;
-				parent = nodes.pop();
-				pindex = indexes.top();
-				indexes.pop();
-			}
+			swap(cur->value[index], parent->value[parent->index - 1]);
+			parent->index -= 1;
+			return true;
 		}
-		else if(lb != NULL)
+		else
 		{
-			pindex -= 1;
-			lb->value[index] = parent->value[pindex];
-			if(CAN_DELETE(lb->index))
-			{
-				parent->value[pindex] = remove(rb, rb->index - 1);
-				cur = NULL;
-			}else
-			{
-				memcpy((void*)(lb->value + lb->index), (void*)(cur->value), cur->index);
-				lb->index += cur->index;
-				delete cur;
-				//检查父节点是否满足要求
-				cur = parent;
-				index = pindex;
-				parent = nodes.pop();
-			}
-		}
-		if(nodes.empty())
-		{
-			if(parent->index > 1)
-			{
-				remove(parent, pindex);
-			}
-			else
-			{
-				root = cur;
-				delete parent;
-			}
-			break;
+			left->value[left->index++] = cur->value[index];
+			memcpy((void*)(left->value + left->index), (void*)(right->value), SIZE(right->index, T));
+			memcpy((void*)(left->child + left->index), (void*)(right->child), SIZE(right->index + 1, bnode*));
+			remove(cur, index);
+			cur->index -= 1;
+			cur->child[cur->index] = left;
+			index = left->index - 1;
+			left->index = left->index + right->index;
+			delete right;
+			parent = cur;
+			cur = left;
 		}
 	}
+	//如果是页节点，直接删除。
+	remove(parent, index);
+	return true;
 }
 /**
  * 二分查找函数，找到返回索引+1，否则返回负值，其相反数是该节点应该插入的地方+1；
@@ -329,7 +291,8 @@ template <class T>
 int btree<T>::binary_search(struct bnode *node, T e)
 {
 	int begin = 0, end = node->index;
-	if(node->index == 0){
+	if (node->index == 0)
+	{
 		return 0;
 	}
 	int middle = MID(begin, end);
@@ -377,6 +340,7 @@ void btree<T>::init_node(bnode *node)
 template <class T>
 void btree<T>::insert(struct bnode *node, int index, T e)
 {
+	node->child[node->index + 1] = node->child[node->index];
 	for (int i = node->index; i > index; i--)
 	{
 		node->value[i] = node->value[i - 1];
@@ -387,24 +351,68 @@ void btree<T>::insert(struct bnode *node, int index, T e)
 }
 
 //函数只能删除叶子节点上的值。
-template<typename T>
+template <typename T>
 T btree<T>::remove(bnode *node, int index)
 {
 	T temp = node->value[index];
-	node->index;
-	for(int i = index; i < node->index; i++)
+	for (int i = index; i < node->index; i++)
 	{
 		node->value[i] = node->value[i + 1];
 	}
-	if(node->child[index] != NULL)
+	if (node->child[index] != NULL)
 	{
-		for(int i = index; i < node->index + 1; i++)
+		for (int i = index; i < node->index + 1; i++)
 		{
 			node->child[i] = node->child[i + 1];
 		}
 	}
 	node->index--;
 	return temp;
+}
+
+template <class T>
+int btree<T>::adjust(bnode *parent, bnode *node, int index)
+{
+	bnode *left = index == 0 ? NULL : parent->child[index - 1];
+	bnode *right = index == parent->index - 1 ? NULL : parent->child[index + 1];
+	if (left != NULL && CAN_DELETE(left->index))
+	{
+		//调整当前节点和左节点的值
+		insert(node, 0, parent->value[index - 1]);
+		parent->value[index - 1] = left->value[--left->index];
+		node->child[0] = left->child[left->index + 1];
+		left->child[left->index + 1] = NULL;
+	}
+	else if (right != NULL && CAN_DELETE(right->index))
+	{
+		//调整当前节点和右节点的值
+		node->value[node->index] = parent->value[index];
+		node->child[++node->index] = right->child[0];
+		parent->value[index] = right->value[0];
+		remove(right, 0);
+	}
+	else if (left != NULL)
+	{
+		//合并当前节点，父节点和左节点
+		index -= 1;
+		left->value[left->index++] = parent->value[index];
+		memcpy((void *)(left->value + left->index), (void *)(node->value), SIZE(node->index, T));
+		memcpy((void *)(left->child + left->index), (void *)(node->child), SIZE(node->index + 1, bnode *));
+		remove(parent, index);
+		parent->child[index] = left;
+		delete node;
+	}
+	else if (right != NULL)
+	{
+		//合并当前节点，父节点和右节点
+		node->value[node->index++] = parent->value[index];
+		memcpy((void *)(node->value + node->index), (void *)(right->value), SIZE(right->index, T));
+		memcpy((void *)(node->child + node->index), (void *)(right->child), SIZE(right->index + 1, bnode *));
+		remove(parent, index);
+		parent->child[index] = node;
+		delete right;
+	}
+	return index;
 }
 } // namespace dbexp
 #endif
--- dbexp2/main.cpp
+++ dbexp2/main.cpp
@@ -4,6 +4,10 @@
 #include "btree.hpp"
 #include "comparitor.hpp"
 
+#ifdef ME_DEBUG
+#define DEBUG_SIZE 40
+#endif
+
 using namespace std;
 using namespace dbexp;
 void readData(btree<itemData> &tree)
@@ -11,6 +15,9 @@ void readData(btree<itemData> &tree)
     ifstream ifile;
     ifile.open("./data.txt", ios::in);
     string line;
+#ifdef ME_DEBUG
+    int count = 0;
+#endif
     while(getline(ifile, line)){
         string index = line.substr(0, line.find_first_of(' '));
         string value = line.substr(line.find_first_of(' ') + 1);
@@ -18,6 +25,12 @@ void readData(btree<itemData> &tree)
         data.index = atoi(index.c_str());
         strcpy(data.value, value.c_str());
         tree.insert(data);
+#ifdef ME_DEBUG
+        count++;
+        if(count > DEBUG_SIZE){
+            break;
+        }
+#endif
     }
 }
 
@@ -26,6 +39,7 @@ void varifyRead(btree<itemData> &tree)
     ifstream ifile;
     ifile.open("./data.txt");
     int count = 0;
+    int lineNum = 0;
     string line;
     while(getline(ifile, line)){
         string index = line.substr(0, line.find_first_of(' '));
@@ -35,18 +49,48 @@ void varifyRead(btree<itemData> &tree)
         strcpy(data.value, value.c_str());
         if(!tree.find(data))
         {
+            cout << "item not find, index is " << index << endl;
             count++;
         }
+        lineNum++;
+#ifdef ME_DEBUG
+        if(lineNum > DEBUG_SIZE){
+            break;
+        }
+#endif
     }
     cout << "totally not find item: " << count << endl;
 }
 
+void testDelete(btree<itemData> &tree)
+{
+    ifstream ifile("./data.txt");
+    string line;
+    int deleted = 0;
+    while(getline(ifile, line))
+    {
+        string index = line.substr(0, line.find_first_of(' '));
+        string value = line.substr(line.find_first_of(' ') + 1);
+        itemData data;
+        data.index = atoi(index.c_str());
+        strcpy(data.value, value.c_str());
+        if(tree.remove(data))
+        {
+            if(tree.find(data))
+            {
+                cout << "unsucceed delete, index " << index << endl;
+            }
+        }
+        deleted++;
+    }
+}
 int main()
 {
     comparitor<itemData> *comp = new itemComparitor();
     btree<itemData> root(comp);
     readData(root);
     varifyRead(root);
+    testDelete(root);
     cout << "finished" << endl;
     return 0;
 }
